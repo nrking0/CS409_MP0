947ca0f1ff75c3eb77f39e9b6b1d04caea96b21d
Nick King <nrking0@gmail.com>
2025-09-01 15:27:37 -0500

    ✅ feat: finish up card/styles/js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
 /* Your JS here. */
-console.log('Hello World!')
+document.getElementById("blue-button").addEventListener("click", function() {
+    document.body.getElementsByClassName("card")[0].style.backgroundColor = "aqua";
+});
+
+document.getElementById("red-button").addEventListener("click", function() {
+    document.body.getElementsByClassName("card")[0].style.backgroundColor = "red";
+});
+
+document.getElementById("green-button").addEventListener("click", function() {
+    document.body.getElementsByClassName("card")[0].style.backgroundColor = "lightgreen";
+});
